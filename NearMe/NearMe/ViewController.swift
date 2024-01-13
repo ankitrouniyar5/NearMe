e7bf1d64b4a09f38d15f24a27dcf9d3ce461d285
Ankit <ankitrouniyar5@gmail.com>
2024-01-13 13:25:58 +0530

    init

---
+++ NearMe/NearMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NearMe
+//
+//  Created by Ankit Rouniyar on 13/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
